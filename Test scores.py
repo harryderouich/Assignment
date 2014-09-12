97f671c3e6697ad5797fed2814b5e11463aead57
harryderouich <40683@student.longroad.ac.uk>
2014-09-12 13:42:02 +0100

    First commit

---
+++ Test scores.py
@@ -0,0 +1,7 @@
+#Harry Derouich
+#12/09/14
+#Exercise 2 - Test Score Addition
+
+score_one = int(input("Please enter the first score: "))
+score_two = int(input("Please enter the second score: "))
+print(score_one+score_two)
