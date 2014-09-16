b3dae876e963616c272403c703d8cf184ea0ef12
harryderouich <40683@student.longroad.ac.uk>
2014-09-16 12:23:52 +0100

    First commit - Finished

---
+++ Height_Weight_Conversion.py
@@ -0,0 +1,15 @@
+#Harry Derouich
+#16/09/14
+#Height and weight conversion
+
+print("Hello User!")
+
+height_inch = int(input("Please enter your height in inches: "))
+weight_stone = int(input("Please enter your weight in stones: "))
+
+height_cm = height_inch * 2.54
+weight_kg = weight_stone * 6.364
+
+print("Your height in centimetres is: {0}, and your weight in kilograms is {1}.".format(height_cm, weight_kg))
+
+      
