3f0b0889ae1733a7a38aa6ef35010f737cd68a4b
harryderouich <40683@student.longroad.ac.uk>
2014-09-19 14:31:26 +0100

    First commit, needs work. Process done just needs output.

---
+++ Quote manipulation.py
@@ -0,0 +1,13 @@
+#Harry Derouich
+#19/09/14
+#Famous Quote Manipulation
+
+quote = input("Please enter a quote by a famous person: ")
+
+quote_upper = quote.upper()
+quote_lower = quote.lower()
+quote_capital = quote.capitalize()
+quote_title = quote.title()
+
+
+print("The quote in Upper case: '{0}'".format(quote_upper))
